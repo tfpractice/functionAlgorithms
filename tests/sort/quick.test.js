66cdf6a60acc3021657c9bfaa5d6edc52d82585a
theory friction practice <jackson.brebnor@gmail.com>
2017-09-07 23:13:12 -0400

    worst case is really bad

--- tests/sort/quick.test.js
+++ tests/sort/quick.test.js
@@ -2,14 +2,18 @@ import 'jasmine-expect';
 import { qSort_orig, quickSort, quickTail } from 'src/sort/quick';
 
 // const test = [ 68, 80, 12, 84, 95, 70, 79, 27, 88, 93 ];
-const ints = Array.from({ length: 300000 }, () =>
-  Math.floor(Math.random() * 300000)
+const ints = Array.from({ length: 100000 }, () =>
+  Math.floor(Math.random() * 100000)
 );
-const tailTest = [ 0, ...new Set(ints) ];
+const tailTest = [ ...new Set([ 0, ...ints ]) ];
+
+// const revTail = tailTest.sort();
 
 // console.log('tailTest', tailTest);
 describe('quickSort', () => {
   it('sorts usnig pivot vals', () => {
     expect(quickTail(tailTest)[0]).toEqual(0);
+
+    // expect(quickTail(revTail)[0]).toEqual(0);
   });
 });
