5479d6d2ca264036083963097d65bd635d2285da
theory friction practice <jackson.brebnor@gmail.com>
2017-09-24 13:23:09 -0400

    spotify questons

---
+++ src/spotify/spotify.js
@@ -0,0 +1,45 @@
+// function groupArtistsByGenre(data) {
+// 	// fill in
+//   const a=0;
+//
+// }
+// const obj2Map=obj=>{
+// let rVal=new Map().set('Hip Hop',[]).set('Pop',[]).set('R&B',[]);
+// let genres=new Set();
+// console.log(Object.entries(obj))
+//
+// let res=Object.values(obj).map(x=> x.map(gr=>genres.add(gr)))
+//
+// console.log(genres)
+//
+// Object.entries(obj).forEach(([a,gs])=>{
+// gs.forEach(g=> rVal.set(g, rVal.get(g).concat(a))
+// console.log(v)
+// })
+// //[...genres].map(g=>{
+//
+//
+// //})
+// }
+// var data = {
+// 	'Kanye': ['Hip Hop'],
+//   'Justin Bieber': ['Hip Hop', 'Pop'],
+//   'Rihanna': ['R&B', 'Hip Hop', 'Pop'],
+//   'Alicia Keys': ['R&B']
+// };
+// obj2Map(data)
+// console.log(groupArtistsByGenre(data));
+//
+// /*
+//
+// Expected output:
+//
+// {
+// 	'Hip Hop': ['Justin Bieber', 'Kanye', 'Rihanna'],
+//   'Pop': ['Justin Bieber', 'Rihanna'],
+//   'R&B': ['Alicia Keys', 'Rihanna'],
+// }
+//
+// */
+//
+//
---
+++ src/spotify/spotify.md
@@ -0,0 +1,22 @@
+Lots of autonomy in the working environment
+Guilds meetup on a monthly basis
+Company-wide hack-week for internal development
+
+three roles
+full-stack
+* JS developer ,vanillajs, unit test, chrome dev tools, getting into the weeds of JS
+* Frontend Generalist, UI/UX/ SEO js compatibles, responsive and performant
+- full stack, working from the database and PHP and JS, hands on design experience, Java and Python is a pluse
+
+
+first 30 minutes are technical questions
+- system designs, breadth of knowledge
+2nd 30 minutes is a coding exercise
+- had to build a stoplight
+- tic-tac-toe
+
+then a ful riund 4 hours 
+- 2 programming
+- 1 system designs
+- 1 with manager
+5hrs
